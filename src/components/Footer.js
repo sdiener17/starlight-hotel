d96314edee32cb7abe13e958e74d7c227c995491
Sarah Diener <sdiener@ksu.edu>
2023-11-19 20:00:48 -0600

    Added files not in previous commits

---
+++ src/components/Footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactAudioPlayer from "react-audio-player";
+import styled from "styled-components";
+// import Mr_Blue_Sky from "../music/Mr_Blue_Sky.mp3";
+
+export default function Footer() {
+  return (
+    <FootWrapper>
+      <div className="FooterMain">
+        <p className="text-1">Copyright 2024</p>
+        {/* <ReactAudioPlayer
+          src={Mr_Blue_Sky}
+          autoPlay={true}
+          controls={true}
+          loop={true}
+          className="audio-player"
+        /> */}
+      </div>
+    </FootWrapper>
+  );
+}
+
+const FootWrapper = styled.footer`
+  //margin-top: 60px;
+  .FooterMain {
+    display: flex;
+    align-items: center;
+    vertical-align: bottom;
+    justify-content: center;
+    flex-direction: row;
+    //max-width: 1600px;
+    //margin: 5 auto;
+    position: -webkit-sticky;
+    position: sticky;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    max-height: 80px;
+    //padding: 1rem 1.5rem;
+    background: linear-gradient(
+      to top right,
+      var(--primarySiteColor),
+      var(--darkSiteColor)
+    );
+    border-bottom: 5px solid var(--mediumGrey);
+    z-index: 999;
+    color: var(--lightGrey);
+  }
+  .text-1 {
+    margin-right: 10px;
+  }
+  .audio-player {
+    max-height: 40px;
+  }
+
+  @media screen and (max-width: 500px) {
+    .FooterMain {
+      float: none;
+      display: block;
+      text-align: left;
+    }
+    .header-right {
+      float: none;
+    }
+  }
+`;
---
+++ src/data/GameContext.js
@@ -0,0 +1,60 @@
+import { createContext, useContext, useReducer } from "react";
+
+const StatsContext = createContext(null);
+const StatsDispatchContext = createContext(null);
+
+export function DataProvider({ children }) {
+  const [stats, dispatch] = useReducer(tasksReducer, initialStats);
+
+  return (
+    <StatsContext.Provider value={stats}>
+      <StatsDispatchContext.Provider value={dispatch}>
+        {children}
+      </StatsDispatchContext.Provider>
+    </StatsContext.Provider>
+  );
+}
+
+export function useStats() {
+  return useContext(StatsContext);
+}
+
+export function useStatsDispatch() {
+  return useContext(StatsDispatchContext);
+}
+
+function tasksReducer(stats, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...stats,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return stats.map((s) => {
+        if (s.id === action.stat.id) {
+          return action.stat;
+        } else {
+          return s;
+        }
+      });
+    }
+    case "deleted": {
+      return stats.filter((s) => s.id !== action.id);
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}
+
+const initialStats = [
+  { id: 0, text: "Philosopher’s Path", done: true },
+  { id: 1, text: "Visit the temple", done: false },
+  { id: 2, text: "Drink matcha", done: false },
+];
